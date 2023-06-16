03ce2867736718ce223554e502d6798cebfdde68
Hardik-hub-prog <77965376+Hardik-hub-prog@users.noreply.github.com>
2023-06-16 16:45:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+
+var typed = new Typed('#mytext' ,{
+
+   strings : ['JS Animation' ,'JS Animation 2'],
+ 
+   typeSpeed: 75  //Property : property value
+
+})
+
+AOS.init({
+
+   duration :2000 //Property : property value
+})
